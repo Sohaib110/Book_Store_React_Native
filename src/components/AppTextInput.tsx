d541f605c56550ca7a26143b075ad399066628ee
Sohaib110 <csscientistsohaib@gmail.com>
2026-01-02 23:29:03 +0500

    add book

--- src/components/AppTextInput.tsx
+++ src/components/AppTextInput.tsx
@@ -1,14 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TextInput, View } from 'react-native'
 import React from 'react'
 
-const AppTextInput = () => {
+const AppTextInput = ({ value, onChangeText, placeholder, keyboardType, ...otherProps }) => {
   return (
-    <View>
-      <Text>AppTextInput</Text>
+    <View style={styles.container}>
+      <TextInput
+        value={value}
+        onChangeText={onChangeText}
+        placeholder={placeholder}
+        keyboardType={keyboardType}
+        {...otherProps}
+      />
     </View>
   )
 }
 
 export default AppTextInput
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#EEEFEE',
+    width: '100%',
+    height: 40,
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    justifyContent: 'center',
+    marginBottom: 20,
+  },
+})
--- src/screens/AddBookScreen.tsx
+++ src/screens/AddBookScreen.tsx
@@ -1,16 +1,47 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import AppTextInput from '../components/AppTextInput';
+import AppButton from '../components/AppButton';
 
 const AddBookScreen = ({ onCloseIconPress }) => {
+
+    const [bookName, setBookName] = useState();
+    const [authorName, setAuthorName] = useState();
+    const [coverURL, setCoverURL] = useState();
+    const [price, setPrice] = useState();
+
     return (
         <SafeAreaView>
             <FontAwesome style={{ margin: 20 }} name="window-close" size={35} color="#E81D1D" onPress={onCloseIconPress} />
+            <View style={styles.body}>
+                <Text style={styles.title}>Book Details</Text>
+                <AppTextInput value={bookName} onChangeText={setBookName} placeholder={"Book Name"} keyboardType={"default"} />
+                <AppTextInput value={authorName} onChangeText={setAuthorName} placeholder={"Author Name"} keyboardType={"default"} />
+                <AppTextInput value={coverURL} onChangeText={setCoverURL} placeholder={"Cover Image"} keyboardType={"default"} />
+                <AppTextInput value={price} onChangeText={setPrice} keyboardType={"numeric"} placeholder={"Price"} />
+                <AppButton onPress={onCloseIconPress} />
+            </View>
+
         </SafeAreaView>
     )
 }
 
 export default AddBookScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    body: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        paddingTop: 30,
+        paddingHorizontal: 20,
+    },
+    title: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
+
+})
